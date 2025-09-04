81e66957ae983aabcd593887033699fde52926e8
AparajitoSaha <aparajito.saha@gmail.com>
2025-09-04 12:14:00 -0400

    [Fix] Indentation bug for camera material script

--- infinigen/assets/materials/dev/invisible_to_camera.py
+++ infinigen/assets/materials/dev/invisible_to_camera.py
@@ -43,10 +43,10 @@ class InvisibleToCamera:
         if not isinstance(obj, list):
             obj = [obj]
 
-        for o in obj:
-            bpy.context.view_layer.objects.active = o
-            o.select_set(True)
-            for i in range(len(o.material_slots)):
-                # bpy.ops.object.material_slot_remove({"object": o})
-                bpy.ops.object.material_slot_remove()
-        surface.add_material(obj, shader_invisible, selection=selection)
+            for o in obj:
+                bpy.context.view_layer.objects.active = o
+                o.select_set(True)
+                for i in range(len(o.material_slots)):
+                    # bpy.ops.object.material_slot_remove({"object": o})
+                    bpy.ops.object.material_slot_remove()
+            surface.add_material(obj, shader_invisible, selection=selection)
